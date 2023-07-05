9105cb380c95dabea224dc1bbe7f90231691590e
Wilson Muita Wanjiru <wilsonmuita41@gmail.com>
2023-07-05 18:01:57 +0300

    <Arithmetic Lab>

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
+// index.js
+// Define the required variables and implement the functions
 
-// Write your code here
+// multiply function
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+
+// random function
+const random = Math.floor(Math.random() * 100) + 1;
+
+// mod function
+const num3 = 17;
+const num4 = 13; // Update the value of num4
+const mod = num3 % num4;
+
+// max function
+const numbers = [10, 5, 20, 15, 7];
+const max = Math.max(...numbers);
